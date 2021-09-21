d66f23a199cb37521fe7f444426bdf8dc0a94b07
ninad <bondre.gunal@gmail.com>
2021-09-21 14:09:42 +0530

    agency portfolio webpage complete

---
+++ script.js
@@ -0,0 +1,29 @@
+// let about = document.querySelector(".about")
+// let leftSection = document.querySelector('.left-section')
+// let rightSection = document.querySelector('.right-section')
+// let services = document.querySelector(".services")
+// let hero = document.querySelector(".hero-section")
+// let line1 = document.querySelector(".line-1")
+// let line2 = document.querySelector(".line-2")
+// let aboutSection = document.querySelector('.about-section')
+// let load = document.querySelector(".loading")
+// about.addEventListener('click', () => {
+//     leftSection.classList.add('active');
+//     rightSection.classList.add('active')
+//     hero.classList.add('active')
+//     about.classList.add('active')
+//     aboutSection.classList.add('active')
+// })
+
+// services.addEventListener('click', () => {
+//     leftSection.classList.add('active');
+//     rightSection.classList.add('active')
+//     hero.classList.add('active')
+
+// })
+
+// function loadfun() {
+//     setTimeout(function() {
+//         load.style.display = "none"
+//     }, 2000);
+// }
